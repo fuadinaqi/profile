1dfcc909e46bebba95fafd79a6901562bf692e9e
fuadi95 <fuadi@qlue.id>
2020-05-14 09:45:21 +0700

    initial commit

---
+++ components/Layout/index.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Container = styled.div``
+
+function Layout({ children }) {
+  return (
+    <Container>
+      {children}
+    </Container>
+  )
+}
+
+export default Layout
