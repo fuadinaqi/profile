cc86959721d734bfafab3626d28632d793ed141a
m.fuadi <m.fuadi@hukumonline.com>
2023-08-03 11:40:47 +0700

    add meta

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,14 @@ export default function Home() {
       <Head>
         <title>About Me | Muhammad Fuadi Naqi</title>
         <link rel="icon" href="/favicon.png" />
+        <meta name="title" content="About Me | Muhammad Fuadi Naqi" />
+        <meta
+          name="description"
+          content=" I am a Frontend Engineer who adheres to the principles of S.O.L.I.D (Single Responsibility, Open-Closed, Liskov Substitution, Interface Segregation, Dependency Inversion)"
+        />
+        <meta name="publisher" content="Fuadi Naqi" />
+        <meta name="author" content="Fuadi Naqi" />
+        <meta name="keywords" content="Fuadi Naqi, Frontend Engineer, React, Next" />
       </Head>
       <ProfileView />
     </>
